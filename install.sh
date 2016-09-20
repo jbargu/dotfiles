9344617f6cf74e65451269802d53a65c96b5c557
Jure Grabnar <jgrabnar@biosistemika.com>
2016-09-20 19:53:15 +0200

    Update install.sh script to be more automatical

--- install.sh
+++ install.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
+
 echo "Installing dotfiles"
+
+cd ~
+
+echo "Install all required programs"
+
+echo "Installing tmux"
+sudo apt-get update -yqqu
+sudo apt-get install -yqqu python-software-properties software-properties-common
+sudo add-apt-repository -yu ppa:pi-rho/dev
+sudo apt-get update -yqqu
+sudo apt-get install -yqqu tmux
+
+sudo apt-get install vim-gnome git zsh wget xclip
+
+git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
+git clone https://github.com/clvv/fasd.git
+cd ~/fasd
+make install
+cd ~
+
+# Install powerline font
+mkdir -p ~/.fonts/
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf -P ~/.fonts
+fc-cache -vf ~/.fonts
+mkdir -p ~/.config/fontconfig/conf.d/
+wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf -P ~/.config/fontconfig/conf.d/
+
+
+# Install vim plugins - now manually
+# Install hub: https://github.com/github/hub/releases
 
 echo "Initializing submodule(s)"
 git submodule update --init --recursive
 
 source install/link.sh
-
-if [ "$(uname)" == "Darwin" ]; then
-    echo "Running on OSX"
-
-    echo "Installing homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-    echo "Brewing all the things"
-    source scripts/brew.sh
-
-    echo "Updating OSX settings"
-    source scripts/osx.sh
-
-    echo "Installing node (from nvm)"
-    nvm install stable
-    nvm alias default stable
-
-    echo "Configuring nginx"
-    # create a backup of the original nginx.conf
-    mv /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/nginx.original
-    ln -s nginx/nginx.conf /usr/local/etc/nginx/nginx.conf
-    # symlink the code.dev from dotfiles
-    ln -s nginx/code.dev /usr/local/etc/nginx/sites-enabled/code.dev
-fi
-
 
 echo "Configuring zsh as default shell"
 chsh -s $(which zsh)
