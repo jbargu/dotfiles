b266d1b2efd3092c633ac5d16158155d6e5fe030
jbargu <jure.grabnar@gmail.com>
2021-05-20 07:49:59 +0200

    Update i3 config

--- i3/polybar/launch.sh
+++ i3/polybar/launch.sh
@@ -9,6 +9,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar top >>/tmp/polybar1.log 2>&1 &
-#polybar bar2 >>/tmp/polybar2.log 2>&1 &
+#polybar bar >>/tmp/polybar2.log 2>&1 &
 
 echo "Bars launched..."
